72a3bcc63a0f0526978d4ce35264d4ff57d435f3
rohit19060 <rohitjain19060@gmail.com>
2022-08-08 18:01:06 +0530

    🤴Methods & Extensions File Added

---
+++ lib/Utils/methods.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+Future<dynamic> networkRequest({
+  required String url,
+  Map<String, String>? formData,
+  bool isPost = false,
+}) async {
+  try {
+    final headers = <String, String>{
+      HttpHeaders.acceptHeader: 'application/json',
+    };
+    if (isPost) {
+      // final List<String> customerData = await SharedPref.getCustomerData();
+      // formData ??= {};
+      // if (customerData.isNotEmpty) {
+      //   formData['customerID'] = customerData[0];
+      //   headers[HttpHeaders.authorizationHeader] = customerData[1];
+      // }
+    }
+    Response response;
+    if (isPost) {
+      response = await post(Uri.parse(url), body: formData, headers: headers);
+    } else {
+      response = await get(Uri.parse(url), headers: headers);
+    }
+    return jsonDecode(response.body);
+  } on TimeoutException {
+    return {
+      'status': 'false',
+      'message': 'The connection has timed out, Please try again!'
+    };
+  } on SocketException {
+    debugPrint('No Internet connection! 😑');
+    return {'status': 'false', 'message': 'No Internet connection! 😑'};
+  } on Exception catch (e) {
+    debugPrint('Request Error: $e');
+    return {'status': 'false', 'message': 'Connection Problem! 😐'};
+  }
+}
