68844010cdd5a4d7d6becc2a02aafd0127b6c98f
Rohit Jain <rohitjain19060@gmail.com>
2022-09-11 11:03:44 +0530

    👑Local Notification Plugin Added

--- lib/Utils/methods.dart
+++ lib/Utils/methods.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart';
 
 Future<dynamic> networkRequest({
@@ -36,4 +38,47 @@ Future<dynamic> networkRequest({
     debugPrint('Request Error: $e');
     return {'status': 'false', 'message': 'Connection Problem! 😐'};
   }
+}
+
+// Local Notification Methods
+final FlutterLocalNotificationsPlugin _notificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void initialize() {
+  const initializationSettingsAndroid = InitializationSettings(
+      android: AndroidInitializationSettings('@drawable/ic_launcher'));
+  _notificationsPlugin.initialize(
+    initializationSettingsAndroid,
+    onSelectNotification: (route) async {
+      if (route != null) {
+        notificationRouter(route, 3);
+      }
+    },
+  );
+}
+
+Future<void> display(RemoteMessage message) async {
+  try {
+    final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+    final notificationDetails = NotificationDetails(
+      android: AndroidNotificationDetails(
+          message.notification!.android!.sound ?? 'Channel Id',
+          message.notification!.android!.sound ?? 'Main Channel',
+          groupKey: 'notificationGroup',
+          color: Colors.blue,
+          importance: Importance.max,
+          sound: RawResourceAndroidNotificationSound(
+              message.notification!.android!.sound),
+          priority: Priority.high),
+    );
+    await _notificationsPlugin.show(id, message.notification?.title,
+        message.notification?.body, notificationDetails,
+        payload: message.data['route'].toString());
+  } on Exception catch (e) {
+    debugPrint(e.toString());
+  }
+}
+
+void notificationRouter(String route, int situation) {
+  // navigatorKey.currentState?.pushNamed(homeRoute, arguments: {'index': 2});
 }
