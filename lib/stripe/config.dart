1994ef35937fe0c4aba1a71f57d770b168bf8896
Rohit Jain <rohitjain19060@gmail.com>
2023-01-28 22:17:47 +0530

    Stripe Updated

--- lib/stripe/config.dart
+++ lib/stripe/config.dart
@@ -1,7 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 // If you are using a real device to test the integration replace this url
 // with the endpoint of your test server (it usually should be the IP of your computer)
-final kApiUrl = defaultTargetPlatform == TargetPlatform.android
-    ? 'http://192.168.1.3:3000/api'
-    : 'http://192.168.1.3:3000/api';
+const kApiUrl = 'http://192.168.1.5:4242';
