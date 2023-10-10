2352e7d968ec474270e04080b9493cb2f38c53a2
Rohit19060 <rohitjain19060@gmail.com>
2023-10-10 13:29:05 +0530

    Phone Pe integration

--- lib/error_handling.dart
+++ lib/error_handling.dart
@@ -4,13 +4,38 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  FlutterError.reportError(FlutterErrorDetails(
+    exception: Exception('FlutterErrorDetails.exception'),
+    stack: StackTrace.current,
+    library: 'FlutterErrorDetails.library',
+    context: ErrorDescription('FlutterErrorDetails.context'),
+    informationCollector: () {
+      print('FlutterErrorDetails.informationCollector');
+      return <DiagnosticsNode>[];
+    },
+    stackFilter: (stack) => stack,
+  ));
+  FlutterError.presentError = (details) async {
+    print('FlutterError: ${details.exception}');
+    print('FlutterError: ${details.context}');
+    print('FlutterError: ${details.informationCollector}');
+    print('FlutterError: ${details.library}');
+    print('FlutterError: ${details.stack}');
+    print('FlutterError: ${details.stackFilter}');
+    print('FlutterError: ${details.silent}');
+  };
   FlutterError.onError = (details) {
+    print('FlutterError: ${details.exception}');
     FlutterError.presentError(details);
     if (kReleaseMode) {
       exit(1);
     }
   };
-  PlatformDispatcher.instance.onError = (error, stack) => true;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    print('PlatformDispatcher: $error');
+    print('PlatformDispatcher: $stack');
+    return false;
+  };
   runApp(const MyApp());
 }
 
